ff1649515c72686145dc3d5073d9ebb236312444
Leslie Lamarche <leslie.lamarche@sri.com>
2026-02-06 01:31:18 -0800

    Functional multicamera map using Don's starmapping files.

---
+++ generate_skymap.py
@@ -0,0 +1,182 @@
+# generate_skymap.py
+
+import numpy as np
+from astropy.io import fits
+from scipy.io import readsav
+import pymap3d as pm
+import h5py
+
+
+#def azel2geo(centlat, centlon, az, el, mapalt_km=110.):
+#    # Radius of earth at poker flat latitude
+#    # In the future, replace this with an ellipsoid formula
+#    # to find earth radius at any latitude
+#    r_e = 6360562
+#    
+#    # Preprocessing az, el: right now they have zeros outside the usable FOV, we replace them with NaNs.
+#    # Everywhere with az=el=0 is replaced with a NaN. This would only mark one valid point as invalid, and
+#    # since it has el=0, it isn't usable anyway.
+#    badrange = np.where((az==0) & (el==0))
+#    az[badrange] = np.nan
+#    el[badrange] = np.nan
+#
+#    # Distance from the ground site to the pixel's light source
+#    # This is just trigonometry!
+#    r = np.sqrt( ( r_e*np.sin(np.pi*el/180) )**2 + 2*(1000*mapalt_km)*r_e + (1000*mapalt_km)**2 ) - r_e*np.sin(np.pi*el/180)
+#
+#    # Converting. Note that fullalt gives a sanity check on how good your projection was
+#    lat,lon,fullalt = pm.aer2geodetic(az,el,r,centlat,centlon,1000*mapalt_km)
+#    lon = normalize_lon(lon, convention='0_360')
+#    lat[~np.isfinite(lat)] = 0.
+#    lon[~np.isfinite(lon)] = 0.
+#
+#    return lat, lon
+
+def azel2geo(site_lat, site_lon, az, el, alt=110.):
+
+    #print(az[az>0.], el[el>0.])
+
+    # lat/lon array
+    x, y, z = pm.geodetic2ecef(site_lat, site_lon, 0.)
+    e, n, u = pm.aer2enu(az, el, 1.)
+    vx, vy, vz = pm.enu2uvw(e, n, u, site_lat, site_lon)
+    #vx, vy, vz = pm.enu2uvw(np.cos(el)*np.sin(az), np.cos(el)*np.cos(az), np.sin(el), site_lat, site_lon)
+
+    earth = pm.Ellipsoid.from_name('wgs84')
+    a2 = (earth.semimajor_axis + alt*1000.)**2
+    b2 = (earth.semimajor_axis + alt*1000.)**2
+    c2 = (earth.semiminor_axis + alt*1000.)**2
+
+    A = vx**2/a2 + vy**2/b2 + vz**2/c2
+    B = x*vx/a2 + y*vy/b2 + z*vz/c2
+    C = x**2/a2 + y**2/b2 + z**2/c2 -1
+
+    alpha = (np.sqrt(B**2-A*C)-B)/A
+
+    lat, lon, alt = pm.ecef2geodetic(x + alpha*vx, y + alpha*vy, z + alpha*vz)
+
+    return lat, lon
+
+
+
+# Normalize longitude values to a specified convention
+def normalize_lon(lon: np.ndarray, convention: str) -> np.ndarray:
+    lon = lon.copy()
+    if convention == "0_360":
+        return np.mod(lon, 360.0)
+    if convention == "-180_180":
+        return (np.mod(lon + 180.0, 360.0) - 180.0)
+    finite = np.isfinite(lon)
+    if not np.any(finite):
+        return lon
+    lon0 = lon[finite]
+    span0 = np.nanmax(lon0) - np.nanmin(lon0)
+    lon_wrapped = (np.mod(lon + 180.0, 360.0) - 180.0)
+    lon1 = lon_wrapped[finite]
+    span1 = np.nanmax(lon1) - np.nanmin(lon1)
+    if span1 < span0:
+        return lon_wrapped
+    return lon
+
+
+# Poker Flat (PKR)
+def load_PKR():
+
+    site_lon, site_lat = [-147.43,   65.1192]
+    
+    azmap = np.rot90(fits.open('PKR_DASC_20220305_Az.FIT')[0].data,3).T
+    elmap = np.rot90(fits.open('PKR_DASC_20220305_El.FIT')[0].data,3).T
+    #azmap = azmap[200:-200,200:-200]
+    #elmap = elmap[200:-200,200:-200]
+
+    #mask = np.where((azmap==0) & (elmap==0))
+    #mask = np.logical_and(azmap==0, elmap==0)
+    mask = elmap<15.
+
+    #import matplotlib.pyplot as plt
+    #plt.pcolormesh(elmap)
+    #plt.colorbar()
+    #plt.show()
+
+    return site_lat, site_lon, azmap, elmap, mask
+
+#glat, glon = azel2geo(site_lat, site_lon, azmap, elmap)
+##print(glat[np.isfinite(glat)], glon[np.isfinite(glon)])
+#
+#with h5py.File('skymap.h5', 'a') as h5:
+#    h5.create_group('PKR')
+#    h5.create_dataset('PKR/latitude', data=glat)
+#    h5.create_dataset('PKR/longitude', data=glon)
+#    h5.create_dataset('PKR/azimuth', data=azmap)
+#    h5.create_dataset('PKR/elevation', data=elmap)
+#    h5.create_dataset('PKR/site_coords', data=[site_lat, site_lon])
+
+
+
+
+
+# Venetie (VEE)
+def load_VEE():
+
+    site_lon, site_lat = [-146.407,  67.013]
+    dat = readsav('VEE_558_latest_az_el_512.sav', python_dict=True)
+    azmap = dat['az_latest_512'].copy()
+    elmap = dat['el_latest_512'].copy()
+
+    ul = [88, 109]
+    lr = [247,288]
+    i0,j0 = ul
+    i1,j1 = lr
+
+    #import matplotlib.pyplot as plt
+    #plt.imshow(azmap)
+    #plt.show()
+
+    ivec = np.arange(i1-i0) + i0
+    m = (j1-j0)/(i1-i0)
+    jvec = m*(ivec-i0) + j0
+    jvec2 = jvec.astype(int)-4
+    jvec3 = jvec.astype(int)+4
+    #plt.plot(ivec,jvec)
+    #plt.plot(ivec,jvec2)
+    #plt.plot(ivec,jvec3)
+
+    for i in ivec:
+        fix_area = azmap[jvec2[i-i0]:jvec3[i-i0],i]
+        fix_area[(fix_area>10.) & (fix_area<350.)] = 0.
+        azmap[jvec2[i-i0]:jvec3[i-i0],i] = fix_area
+
+    #plt.imshow(azmap)
+    #plt.show()
+
+    #mask = np.where((azmap==0) & (elmap==0))
+    #mask = np.logical_and(azmap<0.0, elmap==0)
+    mask = elmap<15.
+
+    return site_lat, site_lon, azmap, elmap, mask
+
+#glat, glon = azel2geo(site_lat, site_lon, azmap, elmap)
+##print(glat[np.isfinite(glat)], glon[np.isfinite(glon)])
+#
+#with h5py.File('skymap.h5', 'a') as h5:
+#    h5.create_group('VEE')
+#    h5.create_dataset('VEE/latitude', data=glat)
+#    h5.create_dataset('VEE/longitude', data=glon)
+#    h5.create_dataset('VEE/azimuth', data=azmap)
+#    h5.create_dataset('VEE/elevation', data=elmap)
+#    h5.create_dataset('VEE/site_coords', data=[site_lat, site_lon])
+
+
+
+# Beaver
+
+site_lon, site_lat = [-147.4,    66.36]
+
+
+
+# Arctic Village
+
+site_lon, site_lat = [-145.533,  68.127]
+
+
+
---
+++ map_asi_realtime_restructure.py
@@ -0,0 +1,333 @@
+#!/usr/bin/env python3
+"""
+map_asi_PKR_realtime.py
+
+This script downloads the latest all-sky imager (ASI) green channel image from the Poker Flat Research Range (PKR) in Alaska,
+maps it to geographic latitude/longitude coordinates using a provided skymap, and overlays rocket trajectories.
+The output is a PNG image showing the mapped green channel intensity and rocket paths.
+
+Usage:
+    python map_asi_PKR_realtime.py --skymap skymap.mat --alt-km 110 --nx 512 --ny 512
+
+Arguments:
+    --skymap        Path to the .mat file containing the geographic mapping for the ASI
+    --alt-km        Altitude (in km) for the mapping grid
+    --nx, --ny      Output grid size in longitude and latitude
+    --padding-deg   Optional: extra padding (in degrees) around the mapped region
+    --lon-convention Optional: longitude format
+"""
+
+# --- Standard imports ---
+import argparse
+import os
+import numpy as np
+import numpy.ma as ma
+import h5py
+from scipy.interpolate import LinearNDInterpolator, NearestNDInterpolator
+import requests
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import magcoordmap as mcm  # Leslie Lamarche's custom module for magnetic grid lines
+from PIL import Image
+from io import BytesIO
+import datetime as dt
+
+import check_intersect as ci
+import generate_skymap as skymap
+
+
+## Create a circular mask for the ASI field of view (optionally with a margin)
+#def circle_mask(h, w, margin_px=0):
+#    yy, xx = np.mgrid[0:h, 0:w]
+#    cy = (h - 1) / 2.0
+#    cx = (w - 1) / 2.0
+#    r = min(h, w) / 2.0 - margin_px
+#    return (xx - cx)**2 + (yy - cy)**2 <= r**2
+
+
+## Load the latitude and longitude mapping arrays from the skymap.mat file for a given altitude
+#def load_skymap_latlon(skymap_path: str, alt_km: int) -> tuple[np.ndarray, np.ndarray]:
+#    key = f"{alt_km}km"
+#    with h5py.File(skymap_path, "r") as f:
+#        g = f["magnetic_footpointing"]
+#        gg = g[key]
+#        lat = np.array(gg["lat"], dtype=float)
+#        lon = np.array(gg["lon"], dtype=float)
+#    return lat, lon
+def load_skymaps():
+    # Load skymaps for all cameras
+    skymaps = dict()
+    lat, lon, az, el, mask = skymap.load_PKR()
+    #az[mask] = np.nan
+    #el[mask] = np.nan
+    skymaps['PKR'] = {'site_lat':lat, 'site_lon':lon, 'azmt':az, 'elev':el, 'mask':mask}
+
+    lat, lon, az, el, mask = skymap.load_VEE()
+    skymaps['VEE'] = {'site_lat':lat, 'site_lon':lon, 'azmt':az, 'elev':el, 'mask':mask}
+
+    for sm in skymaps.values():
+        lat, lon = skymap.azel2geo(sm['site_lat'], sm['site_lon'], sm['azmt'], sm['elev'], alt=110.)
+        #lat[sm['mask']] = np.nan 
+        #lon[sm['mask']] = np.nan 
+        sm['lat'] = lat
+        sm['lon'] = lon
+
+    return skymaps
+
+
+
+## Normalize longitude values to a specified convention
+#def normalize_lon(lon: np.ndarray, convention: str) -> np.ndarray:
+#    lon = lon.copy()
+#    if convention == "0_360":
+#        return np.mod(lon, 360.0)
+#    if convention == "-180_180":
+#        return (np.mod(lon + 180.0, 360.0) - 180.0)
+#    finite = np.isfinite(lon)
+#    if not np.any(finite):
+#        return lon
+#    lon0 = lon[finite]
+#    span0 = np.nanmax(lon0) - np.nanmin(lon0)
+#    lon_wrapped = (np.mod(lon + 180.0, 360.0) - 180.0)
+#    lon1 = lon_wrapped[finite]
+#    span1 = np.nanmax(lon1) - np.nanmin(lon1)
+#    if span1 < span0:
+#        return lon_wrapped
+#    return lon
+
+
+## Create a regular output grid in lat/lon for remapping the ASI data
+#def make_target_grid(lat: np.ndarray, lon: np.ndarray, nx: int, ny: int, padding_deg: float = 0.0):
+#    good = np.isfinite(lat) & np.isfinite(lon)
+#    lat_min, lat_max = np.nanmin(lat[good]), np.nanmax(lat[good])
+#    lon_min, lon_max = np.nanmin(lon[good]), np.nanmax(lon[good])
+#    lat_vec = np.linspace(lat_min - padding_deg, lat_max + padding_deg, ny)
+#    lon_vec = np.linspace(lon_min - padding_deg, lon_max + padding_deg, nx)
+#    Lon, Lat = np.meshgrid(lon_vec, lat_vec)
+#    return Lon, Lat, lon_vec, lat_vec
+#
+#
+## Interpolate scattered ASI data (in image coordinates) onto the regular lat/lon grid
+#def remap_scattered_to_grid(values_2d: np.ndarray,
+#                            lat_2d: np.ndarray,
+#                            lon_2d: np.ndarray,
+#                            Lon: np.ndarray,
+#                            Lat: np.ndarray,
+#                            fill_nearest: bool = True) -> np.ndarray:
+#    pts = np.column_stack([lon_2d.ravel(), lat_2d.ravel()])
+#    vals = values_2d.ravel()
+#    good = np.isfinite(pts).all(axis=1) & np.isfinite(vals)
+#    pts = pts[good]
+#    vals = vals[good]
+#    lin = LinearNDInterpolator(pts, vals, fill_value=np.nan)
+#    out = lin(Lon, Lat)
+#    if fill_nearest:
+#        nanmask = np.isnan(out)
+#        if np.any(nanmask):
+#            nn = NearestNDInterpolator(pts, vals)
+#            out[nanmask] = nn(Lon[nanmask], Lat[nanmask])
+#    return out.astype(np.float32)
+
+def main():
+    # --- Parse command-line arguments ---
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--skymap", default="/Users/anniepflaum/ASI_mapping/skymap.mat")
+    ap.add_argument("--alt-km", type=int, default=110)
+    ap.add_argument("--nx", type=int, default=512)
+    ap.add_argument("--ny", type=int, default=512)
+    ap.add_argument("--padding-deg", type=float, default=0.0)
+    ap.add_argument("--lon-convention", choices=["native", "-180_180", "0_360"], default="native")
+    args = ap.parse_args()
+
+
+#    # --- Load the geographic mapping for the ASI image ---
+#    lat, lon = load_skymap_latlon(args.skymap, args.alt_km)
+#    lon = normalize_lon(lon, args.lon_convention)
+    skymaps = load_skymaps()
+    for k, v in skymaps.items():
+        print(k, v.keys())
+        print(v['lon'][0,0])
+        plt.pcolormesh(v['lon'])
+        plt.colorbar()
+        plt.show()
+
+
+#    # --- Load geographic mapping for PKR ---
+#    filename = '/Users/e30737/Desktop/Research/SoP_DI/ASIspecinvert_test/starcal/PKR_pixel_coords.h5'
+#    site_lon = -147.43
+#    site_lat = 65.1192
+#    with h5py.File(filename, 'r') as h5:
+#        lat = h5['Latitude'][:]
+#        lon = h5['Longitude'][:]
+#        azmt = h5['Azimuth'][:]
+#        elev = h5['Elevation'][:]
+#        mask = h5['Mask'][:]
+#
+#    # --- Load geographic mapping for VEE ---
+#    filename = '/Users/e30737/Desktop/Research/SoP_DI/ASIspecinvert_test/starcal/VEE_pixel_coords.h5'
+#    site_lonv = -146.407
+#    site_latv = 67.013
+#    with h5py.File(filename, 'r') as h5:
+#        latv = h5['Latitude'][:]
+#        lonv = h5['Longitude'][:]
+#        azmtv = h5['Azimuth'][:]
+#        elevv = h5['Elevation'][:]
+#        maskv = h5['Mask'][:]
+
+
+    # --- Calculate mask for overlaping images
+    mp, mv = ci.calculate_masks(skymaps['PKR']['site_lat'], skymaps['PKR']['site_lon'], skymaps['PKR']['azmt'], skymaps['PKR']['elev'], skymaps['VEE']['site_lat'], skymaps['VEE']['site_lon'], skymaps['VEE']['azmt'], skymaps['VEE']['elev'])
+
+    skymaps['PKR']['mask'] = np.logical_or(skymaps['PKR']['mask'], mp)
+    skymaps['VEE']['mask'] = np.logical_or(skymaps['VEE']['mask'], mv)
+
+#    mask = np.logical_or(mask, m)
+#    maskv = np.logical_or(maskv, mv)
+
+    
+    # SOME KIND OF LOOP HERE UPDATING DATA EVERY 15 SEC
+    #img = update_images()
+
+    imgs = dict()
+
+    # --- Download the latest PKR green channel image (already single-channel) ---
+    url = "https://optics.gi.alaska.edu/realtime/latest/pkr_latest_green.jpg"
+    print(f"Downloading {url} ...")
+    resp = requests.get(url, verify=False)  # verify=False disables SSL cert check (safe for public data)
+    resp.raise_for_status()
+    img = Image.open(BytesIO(resp.content))
+    img = np.asarray(img)
+    if img.ndim == 3:
+        # If image is RGB, take only one channel (shouldn't be needed, but for safety)
+        img = img[:, :, 0]
+    #img = img[200:-200,200:-200]
+    imgs['PKR'] = img.astype(np.float32)
+    #img = img.astype(np.float32)
+    #H, W = img.shape
+
+    # --- Download the latest VEE green channel image (already single-channel) ---
+    # NOTE: This will only work after we get new skymap files from DH.  The one LL provided does not work with the realtime images.
+    #url = "https://optics.gi.alaska.edu/amisr_archive/VEE/GASI_5577/png/20260203/VEE_558_20260203_092814.png"
+    #url = 'https://optics.gi.alaska.edu/realtime/latest/vee_latest.jpg'
+    url = 'https://optics.gi.alaska.edu/realtime/latest/vee_558_latest.jpg'
+    print(f"Downloading {url} ...")
+    resp = requests.get(url, verify=False)  # verify=False disables SSL cert check (safe for public data)
+    resp.raise_for_status()
+    imgv = Image.open(BytesIO(resp.content))
+    imgv = np.asarray(imgv)
+    if imgv.ndim == 3:
+        # If image is RGB, take only one channel (shouldn't be needed, but for safety)
+        imgv = imgv[:, :, 0]
+    imgs['VEE'] = imgv.astype(np.float32)
+#    imgv = imgv.astype(np.float32)
+#    Hv, Wv = imgv.shape
+
+
+#    # --- Mask out pixels outside the ASI field of view and invalid mapping points ---
+#    fov = circle_mask(H, W, margin_px=50) & np.isfinite(lat) & np.isfinite(lon)
+#    lat_use = np.where(fov, lat, np.nan)
+#    lon_use = np.where(fov, lon, np.nan)
+#
+#    # --- Create the output lat/lon grid ---
+#    Lon, Lat, lon_vec, lat_vec = make_target_grid(lat_use, lon_use, args.nx, args.ny, args.padding_deg)
+#    fill_nearest = False
+#
+#    # --- Determine which output grid points are inside the valid ASI footprint ---
+#    footprint = remap_scattered_to_grid(fov.astype(np.float32), lat_use, lon_use, Lon, Lat, fill_nearest=False)
+#    inside = np.isfinite(footprint) & (footprint > 0.5)
+#    footprint = inside.astype(np.float32)
+#    Lon, Lat = Lon.astype(np.float32), Lat.astype(np.float32)
+    #img[mask] = np.nan
+    #imgv[maskv] = np.nan
+
+    #for site, img in imgs.items():
+    #    img[skymaps[site]['mask']] = np.nan
+
+
+    # --- Set up the Cartopy map for plotting ---
+    proj = ccrs.AlbersEqualArea(central_longitude=-154, central_latitude=55, standard_parallels=(55, 65))
+    fig = plt.figure(figsize=(6, 5))
+    ax = plt.axes(projection=proj)
+    ax.set_extent([-170, -140, 57, 72], crs=ccrs.PlateCarree())
+    ax.add_feature(cfeature.LAND.with_scale("50m"), zorder=0)
+    ax.add_feature(cfeature.OCEAN.with_scale("50m"), zorder=0)
+    ax.add_feature(cfeature.COASTLINE.with_scale("50m"), linewidth=0.8, zorder=2)
+    ax.add_feature(cfeature.STATES.with_scale("50m"), linewidth=0.5, zorder=2)
+    #extent = (float(np.nanmin(Lon)), float(np.nanmax(Lon)), float(np.nanmin(Lat)), float(np.nanmax(Lat)))
+    ax.set_title("PKR ASI latest green channel mapped to geographic lat/lon")
+    mgl = mcm.maggridlines(ax)
+
+
+    # --- Overlay rocket trajectories from text files (if available) ---
+    def load_traj(filename):
+        # Load latitude and longitude columns from a trajectory text file
+        data = np.loadtxt(filename, skiprows=1, usecols=(1,2))
+        lats, lons = data[:,0], data[:,1]
+        return lats, lons
+
+    try:
+        #lat1, lon1 = load_traj('/Users/anniepflaum/ASI_mapping/Traj_LeftGneissDec25.txt')
+        #lat2, lon2 = load_traj('/Users/anniepflaum/ASI_mapping/Traj_RightGneissDec25.txt')
+        lat1, lon1 = load_traj('../Traj_Left.txt')
+        lat2, lon2 = load_traj('../Traj_Right.txt')
+        #ax.scatter(lon1, lat1, s=0.5, color='red', label='GNEISS trajectory', transform=ccrs.PlateCarree(), zorder=3)
+        #ax.scatter(lon2, lat2, s=0.5, color='red', transform=ccrs.PlateCarree(), zorder=3)
+        ax.plot(lon1, lat1, color='red', label='GNEISS trajectory', transform=ccrs.PlateCarree(), zorder=3)
+        ax.plot(lon2, lat2, color='red', transform=ccrs.PlateCarree(), zorder=3)
+        ax.legend(loc='upper right')
+    except Exception as e:
+        print(f"Could not plot rocket trajectories: {e}")
+
+
+    ## --- Remap the green channel image to the geographic grid ---
+    #mapped = remap_scattered_to_grid(img, lat_use, lon_use, Lon, Lat, fill_nearest=fill_nearest)
+    #mapped[~inside] = np.nan  # Mask out-of-footprint pixels
+    #norm_green = np.clip(mapped / 255.0, 0, 1)  # Normalize to [0, 1] for display
+    #mask = np.isfinite(mapped)
+    #im_handle = ax.imshow(norm_green, origin="lower", extent=extent, transform=ccrs.PlateCarree(), zorder=1, alpha=mask.astype(float), cmap='viridis')
+
+    for site, img in imgs.items():
+        print(site)
+        #if site == 'PKR':
+        #    continue
+
+        img[skymaps[site]['mask']] = np.nan
+        #masked_img = ma.masked_where(skymaps[site]['mask'], img)
+
+        print(skymaps[site]['lon'].shape, skymaps[site]['lat'].shape, img.shape, skymaps[site]['mask'].shape)
+
+        #fig, ax1 = plt.subplots()
+        #c = ax1.pcolormesh(skymaps[site]['lon'], skymaps[site]['lat'], img, shading='nearest', vmin=22.5, vmax=42.5)
+        ##c = ax1.pcolormesh(skymaps[site]['lat'], vmin=60, vmax=70)
+        #fig.colorbar(c)
+        #plt.show()
+
+        #im_handle = ax.pcolormesh(skymaps[site]['lon'], skymaps[site]['lat'], img, transform=ccrs.PlateCarree())
+        img_flat = img[~skymaps[site]['mask']].flatten()
+        lon_flat = skymaps[site]['lon'][~skymaps[site]['mask']].flatten()
+        lat_flat = skymaps[site]['lat'][~skymaps[site]['mask']].flatten()
+
+        im_handle = ax.tripcolor(lon_flat, lat_flat, img_flat, transform=ccrs.PlateCarree())
+        #im_handle = ax.contourf(skymaps[site]['lon'], skymaps[site]['lat'], img, transform=ccrs.PlateCarree())
+        #im_handle = ax.pcolor(skymaps[site]['lon'], skymaps[site]['lat'], img, transform=ccrs.PlateCarree())
+
+        #fig, ax1 = plt.subplots()
+        #im_handle = ax1.pcolor(skymaps[site]['lon'], skymaps[site]['lat'], img)
+        #fig.colorbar(im_handle)
+        #plt.show()
+
+
+    txt = ax.text(0.99, 0.01, dt.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), transform=ax.transAxes, fontsize=12, color='w', ha='right', va='bottom', bbox=dict(facecolor='black', alpha=0.5, boxstyle='round,pad=0.2'))
+
+    # --- Add colorbar and save the figure ---
+    cbar = plt.colorbar(im_handle, ax=ax, orientation='vertical', pad=0.02, fraction=0.05)
+    cbar.set_label('Green channel intensity (normalized)')
+    plt.tight_layout()
+    output_path = "PKR_realtime.png"
+    plt.savefig(output_path, dpi=150)
+    print(f"Saved mapped image to {output_path}")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
