846065ef0b90c8aa66d2fa4a6cd745c4df898c59
Leslie Lamarche <leslie.lamarche@sri.com>
2026-02-09 18:16:43 -0800

    Add command line option for fast vs pretty.

--- map_asi_realtime_restructure.py
+++ map_asi_realtime_restructure.py
@@ -151,59 +151,69 @@ def retrieve_pfisr():
     vv[ve>vm,:] = [np.nan, np.nan, np.nan]
     vm[ve>vm] = np.nan
 
-    return ne, glat, glon, vv, vm, vlat, vlon
+    pfisr_data = {'ne':ne, 'glat':glat, 'glon':glon, 'vel':vv, 'mag':vm, 'vlat':vlat, 'vlon':vlon}
+
+    #return ne, glat, glon, vv, vm, vlat, vlon
+    return pfisr_data
 
 
 
 ####### PLOTTING############
 
 
-def plot_fast(skymaps, imgs):
+def plot_fast(skymaps, imgs, pfisr):
 
+    coastlons = np.loadtxt('coastlon.txt')
+    coastlats = np.loadtxt('coastlat.txt')
 
-    # FAST
+    # Plot setup
     fig = plt.figure(figsize=(15, 10))
     gs = gridspec.GridSpec(4,4, width_ratios=[4,0.2,0.2,1])
     ax = fig.add_subplot(gs[:,0])
-
-    # generic setup
-    ax.set_title("PKR ASI latest green channel mapped to geographic lat/lon")
-    ax.legend(loc='upper right')
-
-    ############## ADD ALASKA #################################
-    coastlons = np.loadtxt('coastlon.txt')
-    coastlats = np.loadtxt('coastlat.txt')
     ax.plot(coastlons,coastlats, color='black')#,s=15) 
-    ##########################################################
-
-    ################### PRETTY STUFF #############################
     ax.set_ylim(ymin=57.5, ymax=72)
     ax.set_xlim(xmin=-170, xmax=-135)
     ax.set_aspect(2.2)
     ax.grid()
-    ##############################################################
     # --- Setup sidebar plots ---
     ax1 = dict()
     for i, site in enumerate(imgs.keys()):
         # FAST
         ax1[site] = fig.add_subplot(gs[i,-1])
-        ax1[site].set_title(site)
-
-    #for ax in ax1.values():
+        ax1[site].plot(coastlons,coastlats, color='black')#,s=15) 
         ax1[site].set_ylim(ymin=57.5, ymax=72)
         ax1[site].set_xlim(xmin=-170, xmax=-135)
         ax1[site].set_aspect(2.2)
-        ax1[site].plot(coastlons,coastlats, color='black')#,s=15) 
         ax1[site].grid()
+        ax1[site].set_title(site)
 
 
 
-    # --- Overlay rocket trajectories from text files (if available) ---
+    for site, img in imgs.items():
+        print(site)
+
+        # apply mask
+        img[skymaps[site]['mask']] = np.nan
+        im = img.copy()
+        for m in skymaps[site]['extra_masks'].values():
+            im[m] = np.nan
+        # Partial FoV for main plots
+        im_handle = ax.pcolor(skymaps[site]['lon'], skymaps[site]['lat'], im)
+        # Full FoV for sidebar plots
+        ax1[site].pcolor(skymaps[site]['lon'], skymaps[site]['lat'], img)
+
+    # --- Overlay PFISR ---
+    print('PFISR')
+    pfisr_handle = ax.scatter(pfisr['glon'], pfisr['glat'], c=pfisr['ne'], zorder=6, cmap='jet')
+    u, v = scale_uv(pfisr['vlon'], pfisr['vlat'], pfisr['vel'][:,0], pfisr['vel'][:,1])
+    qp = ax.quiver(pfisr['vlon'], pfisr['vlat'], u, v, zorder=7, scale=5000, width=0.005)
+
+
+    # --- Overlay rocket trajectories from text files ---
+    print('Trajectories')
     lat1, lon1, latm1, lonm1, lata1, lona1 = load_traj('Traj_Left.txt')
     lat2, lon2, latm2, lonm2, lata2, lona2 = load_traj('Traj_Right.txt')
 
-
-    # FAST
     ax.plot(lon1, lat1, color='red', label='GNEISS trajectory',zorder=7)
     ax.scatter(lonm1, latm1, color='red', s=15, zorder=7)
     ax.scatter(lona1, lata1, color='magenta', marker='x', zorder=7)
@@ -213,38 +223,12 @@ def plot_fast(skymaps, imgs):
 
 
 
-    for site, img in imgs.items():
-        print(site)
-
-        # apply mask
-        img[skymaps[site]['mask']] = np.nan
-
-        im = img.copy()
-        lat = skymaps[site]['lat'].copy()
-        lon = skymaps[site]['lon'].copy()
-        for m in skymaps[site]['extra_masks'].values():
-            im[m] = np.nan
-
-
-        # FAST
-        im_handle = ax.pcolor(skymaps[site]['lon'], skymaps[site]['lat'], im)
-        ax1[site].pcolor(skymaps[site]['lon'], skymaps[site]['lat'], img)
-        ax.plot(skymaps[site]['site_lon'],skymaps[site]['site_lat'],color='m', zorder=5)
-
-
-
-    # MOVE TO MAIN
-    ne, glat, glon, vv, vm, vlat, vlon = retrieve_pfisr()
-    pfisr_handle = ax.scatter(glon, glat, c=ne, zorder=6, cmap='jet')
-
-    u, v = scale_uv(vlon, vlat, vv[:,0], vv[:,1])
-    qp = ax.quiver(vlon, vlat, u, v, zorder=7, scale=5000, width=0.005)
-
 
 
     # Final output plot
     txt = ax.text(0.99, 0.01, dt.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), transform=ax.transAxes, fontsize=12, color='w', ha='right', va='bottom', bbox=dict(facecolor='black', alpha=0.5, boxstyle='round,pad=0.2'))
-
+    ax.set_title("PKR ASI latest green channel mapped to geographic lat/lon")
+    ax.legend(loc='upper right')
 
     # --- Add colorbar and save the figure ---
     ax.quiverkey(qp, 0.1, 0.9, 500., '500 m/s', transform=ax.transAxes)
@@ -255,6 +239,8 @@ def plot_fast(skymaps, imgs):
     cbar = fig.colorbar(pfisr_handle, cax=cax, orientation='vertical')
     cbar.set_label(r'Electron Density (m$^{-3}$)')
     plt.tight_layout()
+
+
     output_path = "PKR_realtime.png"
     plt.savefig(output_path, dpi=150)
     print(f"Saved mapped image to {output_path}")
@@ -264,7 +250,7 @@ def plot_fast(skymaps, imgs):
 
 
 
-def plot_pretty(skymaps, imgs):
+def plot_pretty(skymaps, imgs, pfisr):
 
     # --- Set up the Cartopy map for plotting ---
     proj = ccrs.AlbersEqualArea(central_longitude=-154, central_latitude=55, standard_parallels=(55, 65))
@@ -277,34 +263,18 @@ def plot_pretty(skymaps, imgs):
     ax.add_feature(cfeature.OCEAN.with_scale("50m"), zorder=0)
     ax.add_feature(cfeature.COASTLINE.with_scale("50m"), linewidth=0.8, zorder=2)
     ax.add_feature(cfeature.STATES.with_scale("50m"), linewidth=0.5, zorder=2)
-    #extent = (float(np.nanmin(Lon)), float(np.nanmax(Lon)), float(np.nanmin(Lat)), float(np.nanmax(Lat)))
-    ax.set_title("PKR ASI latest green channel mapped to geographic lat/lon")
+    ax.gridlines()
     mgl = mcm.maggridlines(ax)
 
     # --- Setup sidebar plots ---
     ax1 = dict()
-    for i, k in enumerate(imgs.keys()):
-        # PRETTY
-        ax1[k] = fig.add_subplot(gs[i,-1], projection=proj)
-        ax1[k].coastlines()
-        ax1[k].gridlines()
-        ax1[k].set_extent([-170, -140, 57, 72], crs=ccrs.PlateCarree())
-
-
-    # --- Overlay rocket trajectories from text files (if available) ---
-    lat1, lon1, latm1, lonm1, lata1, lona1 = load_traj('Traj_Left.txt')
-    lat2, lon2, latm2, lonm2, lata2, lona2 = load_traj('Traj_Right.txt')
-
-
-    # PRETTY
-    ax.plot(lon1, lat1, color='red', label='GNEISS trajectory', transform=ccrs.PlateCarree(), zorder=7)
-    ax.scatter(lonm1, latm1, color='red', s=15, transform=ccrs.PlateCarree(), zorder=7)
-    ax.scatter(lona1, lata1, color='magenta', marker='x', label='Apogee', transform=ccrs.PlateCarree(), zorder=8)
-    ax.plot(lon2, lat2, color='red', transform=ccrs.PlateCarree(), zorder=7)
-    ax.scatter(lonm2, latm2, color='red', s=15, transform=ccrs.PlateCarree(), zorder=7)
-    ax.scatter(lona2, lata2, color='magenta', marker='x', transform=ccrs.PlateCarree(), zorder=8)
-    ax.legend(loc='upper right')
-
+    for i, site in enumerate(imgs.keys()):
+        ax1[site] = fig.add_subplot(gs[i,-1], projection=proj)
+        ax1[site].coastlines()
+        ax1[site].gridlines()
+        mcm.maggridlines(ax1[site])
+        ax1[site].set_extent([-170, -140, 57, 72], crs=ccrs.PlateCarree())
+        ax1[site].set_title(site)
 
 
 
@@ -326,33 +296,42 @@ def plot_pretty(skymaps, imgs):
         lon_flat = skymaps[site]['lon'][~skymaps[site]['mask']].flatten()
         lat_flat = skymaps[site]['lat'][~skymaps[site]['mask']].flatten()
 
-        im_handle = ax1[site].tripcolor(lon_flat, lat_flat, img_flat, vmin=0, vmax=60, zorder=3, transform=ccrs.PlateCarree())
+        im_handle = ax1[site].tripcolor(lon_flat, lat_flat, img_flat, zorder=3, transform=ccrs.PlateCarree())
 
         imf = im[np.isfinite(im)].flatten()
         latf = lat[np.isfinite(im)].flatten()
         lonf = lon[np.isfinite(im)].flatten()
 
-        #ax1[site].tripcolor(lon_flat, lat_flat, img_flat, transform=ccrs.PlateCarree())
-        ax.tripcolor(lonf, latf, imf, vmin=0, vmax=60, transform=ccrs.PlateCarree())
-        ax1[site].set_title(site)
+        ax.tripcolor(lonf, latf, imf, transform=ccrs.PlateCarree())
         #############################
 
 
-    # PFISR
-    ne, glat, glon, vv, vm, vlat, vlon = retrieve_pfisr()
-    pfisr_handle = ax.scatter(glon, glat, c=ne, zorder=6, cmap='jet', transform=ccrs.Geodetic())
-    #pfisr_handle = ax.scatter(glon, glat, c=ne, zorder=6, cmap='jet')
 
-    u, v = scale_uv(vlon, vlat, vv[:,0], vv[:,1])
-    #qp = ax.quiver(vlon, vlat, u, v, zorder=7, scale=5000, width=0.005)
-    qp = ax.quiver(vlon, vlat, u, v, zorder=7, scale=5000, width=0.005, transform=ccrs.PlateCarree())
-    #qp = ax.quiver(vlon, vlat, u, v, vm, norm=mpl.colors.Normalize(vmin=0,vmax=1000), cmap='cubehelix', zorder=7, scale=5000, width=0.005, transform=ccrs.PlateCarree())
+
+    # --- Overlay PFISR ---
+    pfisr_handle = ax.scatter(pfisr['glon'], pfisr['glat'], c=pfisr['ne'], zorder=6, cmap='jet', transform=ccrs.Geodetic())
+    u, v = scale_uv(pfisr['vlon'], pfisr['vlat'], pfisr['vel'][:,0], pfisr['vel'][:,1])
+    qp = ax.quiver(pfisr['vlon'], pfisr['vlat'], u, v, zorder=7, scale=5000, width=0.005, transform=ccrs.PlateCarree())
 
 
 
+    # --- Overlay rocket trajectories from text files ---
+    lat1, lon1, latm1, lonm1, lata1, lona1 = load_traj('Traj_Left.txt')
+    lat2, lon2, latm2, lonm2, lata2, lona2 = load_traj('Traj_Right.txt')
+
+    ax.plot(lon1, lat1, color='red', label='GNEISS trajectory', transform=ccrs.PlateCarree(), zorder=7)
+    ax.scatter(lonm1, latm1, color='red', s=15, transform=ccrs.PlateCarree(), zorder=7)
+    ax.scatter(lona1, lata1, color='magenta', marker='x', label='Apogee', transform=ccrs.PlateCarree(), zorder=8)
+    ax.plot(lon2, lat2, color='red', transform=ccrs.PlateCarree(), zorder=7)
+    ax.scatter(lonm2, latm2, color='red', s=15, transform=ccrs.PlateCarree(), zorder=7)
+    ax.scatter(lona2, lata2, color='magenta', marker='x', transform=ccrs.PlateCarree(), zorder=8)
+
+
+
+    # Final plotting
+    ax.set_title("PKR ASI latest green channel mapped to geographic lat/lon")
     txt = ax.text(0.99, 0.01, dt.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), transform=ax.transAxes, fontsize=12, color='w', ha='right', va='bottom', bbox=dict(facecolor='black', alpha=0.5, boxstyle='round,pad=0.2'))
-
-
+    ax.legend(loc='upper right')
 
     # --- Add colorbar and save the figure ---
     ax.quiverkey(qp, 0.1, 0.9, 500., '500 m/s', transform=ax.transAxes)
@@ -377,31 +356,20 @@ def main():
 
     # --- Parse command-line arguments ---
     ap = argparse.ArgumentParser()
-    ap.add_argument("--skymap", default="/Users/anniepflaum/ASI_mapping/skymap.mat")
-    ap.add_argument("--alt-km", type=int, default=110)
-    ap.add_argument("--nx", type=int, default=512)
-    ap.add_argument("--ny", type=int, default=512)
-    ap.add_argument("--padding-deg", type=float, default=0.0)
-    ap.add_argument("--lon-convention", choices=["native", "-180_180", "0_360"], default="native")
+    ap.add_argument("--pretty", action='store_true')
     args = ap.parse_args()
 
 
-#    # --- Load the geographic mapping for the ASI image ---
+    # --- Load the geographic mapping for the ASI image ---
     skymaps = load_skymaps()
 
 
 
     # --- Calculate mask for overlaping images
-    # need to generalize
-    #mp, mv = ci.calculate_masks(skymaps['PKR']['site_lat'], skymaps['PKR']['site_lon'], skymaps['PKR']['azmt'], skymaps['PKR']['elev'], skymaps['VEE']['site_lat'], skymaps['VEE']['site_lon'], skymaps['VEE']['azmt'], skymaps['VEE']['elev'])
-    #skymaps['PKR']['mask'] = np.logical_or(skymaps['PKR']['mask'], mp)
-    #skymaps['VEE']['mask'] = np.logical_or(skymaps['VEE']['mask'], mv)
     sites = list(skymaps.keys())
     for s0 in sites:
         red_sites = sites.copy()
         red_sites.remove(s0)
-        print(s0)
-        print(red_sites)
         skymaps[s0]['extra_masks'] = dict()
         for s1 in red_sites:
             m0, m1 = ci.calculate_masks(skymaps[s0]['site_lat'], skymaps[s0]['site_lon'], skymaps[s0]['azmt'], skymaps[s0]['elev'], skymaps[s1]['site_lat'], skymaps[s1]['site_lon'], skymaps[s1]['azmt'], skymaps[s1]['elev'])
@@ -425,21 +393,21 @@ def main():
 
     # --- Download the latest BVR green channel image (already single-channel) ---
     url = 'https://optics.gi.alaska.edu/realtime/latest/bvr_558_latest.jpg'
-    #url = 'https://optics.gi.alaska.edu/amisr_archive/BVR/GASI_5577/png/20260208/BVR_558_20260208_110302.png'
-    #imgs['BVR'] = retrieve_image(url)
     im = retrieve_image(url)
     imgs['BVR'] = np.flipud(im)
 
 
     # --- Download the latest PKR green channel image (already single-channel) ---
     url = "https://optics.gi.alaska.edu/realtime/latest/pkr_latest_green.jpg"
-    #url = "https://optics.gi.alaska.edu/amisr_archive/PKR/DASC/jpg/2026/20260208/PKR_DASC_0558_20260208_110311.0000.JPEG"
     imgs['PKR'] = retrieve_image(url)
 
-    if False:
-        plot_pretty(skymaps, imgs)
+
+    pfisr = retrieve_pfisr()
+
+    if args.pretty:
+        plot_pretty(skymaps, imgs, pfisr)
     else:
-        plot_fast(skymaps, imgs)
+        plot_fast(skymaps, imgs, pfisr)
 
 
     tocall=time.time()
