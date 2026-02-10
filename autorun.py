082f12be4c0daf55ef776aa9709ef9f064e0d0a9
Leslie Lamarche <leslie.lamarche@sri.com>
2026-02-10 00:48:04 -0800

    Add latest pretty update.

--- autorun.py
+++ autorun.py
@@ -18,8 +18,8 @@ def job_fast():
 def job_pretty():
     os.system('python map_asi_realtime_restructure.py --pretty')
     os.system('rsync -av -e "ssh -i ~/.ssh/gioptics-key" --progress ../launch_science_pretty/ realtime@optics.gi.alaska.edu:/import/amisr/archive/Processed_data/GNEISS/mapped_ASI/pretty')
-    #most_recent = sorted(os.listdir('../launch_science_fast'))[-1]
-    #os.system(f'scp ../launch_science_fast/{most_recent} gioptics:/var/www/html/realtime/latest/gneiss_mapped/gneiss_mapped_fast_latest.png')
+    most_recent = sorted(os.listdir('../launch_science_pretty'))[-1]
+    os.system(f'scp ../launch_science_pretty/{most_recent} gioptics:/var/www/html/realtime/latest/gneiss_mapped/gneiss_mapped_pretty_latest.png')
 
 schedule.every(refresh_rate).seconds.do(job_fast)
 schedule.every(5).minutes.do(job_pretty)
