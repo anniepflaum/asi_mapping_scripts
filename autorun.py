2d3cdf783f92b1c95f4e1f1e4315ebb7640fbbf4
Leslie Lamarche <leslie.lamarche@sri.com>
2026-02-09 23:21:58 -0800

    Add copying latest to optics.gi.

--- autorun.py
+++ autorun.py
@@ -11,7 +11,10 @@ refresh_rate = 30.
 def job():
     os.system('python map_asi_realtime_restructure.py')
     os.system('rsync -av -e "ssh -i ~/.ssh/gioptics-key" --progress ../launch_science_fast/ realtime@optics.gi.alaska.edu:/import/amisr/archive/Processed_data/GNEISS/mapped_ASI/fast')
+    most_recent = sorted(os.listdir('../launch_science_fast'))[-1]
+    os.system(f'scp ../launch_science_fast/{most_recent} gioptics:/var/www/html/realtime/latest/gneiss_mapped/gneiss_mapped_fast_latest.png')
 
+job()
 
 schedule.every(refresh_rate).seconds.do(job)
 
